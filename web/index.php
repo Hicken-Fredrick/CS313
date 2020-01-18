ecec45e11dd82780b217faaf595cddfba9ca865c
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-01-18 07:08:42 -0700

    test

--- web/index.php
+++ web/index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CS313 | Hello World | Fred Hicken</title>
+    <meta name="author" content="Fred Hicken">
+    <meta name="description" content="Hello World!">
+</head>
+<body>
+  <h1 style="color:orange;text-align:center;">HELLO WORLD!</h1>
+</body>
+</html>
+
 <?php
 
   phpinfo();
