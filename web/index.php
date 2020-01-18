4d711454b30ce985c7a55764e3c7b16f4e31110b
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-01-18 07:52:02 -0700

    test

--- web/index.php
+++ web/index.php
@@ -6,7 +6,19 @@
     <meta name="description" content="Index Page">
 </head>
 <body>
-  <h1 style="color:orange;text-align:center;">HELLO WORLD!</h1>
+  <h1>Index Page</h1>
+  <h2><a href="#">Personal Information</a></h2>
+  <ul>
+    <li>Assignments</li>
+    <ul>
+      <li>Coming Soon&copy;</li>
+    </ul>
+    <li>Team Work</li>
+    <ul>
+      <li><a href="week01/week01.html">Week 01 - Basic Jquery</a></li>
+      <li>More Coming Soon&copy;</li>
+    </ul>
+  </ul>
 </body>
 <footer>
 <?php include 'footer.php'; ?>
