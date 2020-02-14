1b58fb9e24b90672af940a2238b2ab8f28cfea41
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-02-14 14:17:34 -0700

    working on inner lists

--- web/prove06/header.php
+++ web/prove06/header.php
@@ -1,6 +1,7 @@
 <?php 
-echo "<meta charset=\"utf-8\">";
-echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-echo "<meta name=\"author\" content=\"Fred Hicken\">";
-echo "<link href=\"normalize.css\" rel=\"stylesheet\">";
+echo '<meta charset="utf-8">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '<meta name="author" content="Fred Hicken">';
+echo '<link href="normalize.css" rel="stylesheet">';
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>';
 ?>
