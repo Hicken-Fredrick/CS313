03a0974bd906ed1711127c1cc46a71b70a5c72cb
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-02-14 14:42:54 -0700

    working on inner lists

--- web/prove06/innerListView.php
+++ web/prove06/innerListView.php
@@ -70,7 +70,6 @@
     echo '</main>';
 
   ?>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="showAdd.js"></script>
 <script src="AJAXAddDB.js"></script>
 </body>
