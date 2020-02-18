ab83b32b13e60dd604be1754b0e854159df90d78
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-02-18 02:59:44 -0700

    working on inner lists

--- web/prove06/deleteListDB.php
+++ web/prove06/deleteListDB.php
@@ -7,7 +7,7 @@ session_start();
 $id = htmlspecialchars($_SESSION['user']);
 $listId = htmlspecialchars($_POST['listid']);
 
-$stmt = $db->prepare('DELETE FROM wishlist.list WHERE listid=:listid & userid=:id');
+$stmt = $db->prepare('DELETE FROM wishlist.list WHERE listid= :listid AND userid= :id');
 $stmt->bindValue(':listid', $listId, PDO::PARAM_INT);
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
