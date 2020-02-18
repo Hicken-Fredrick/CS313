59bc0c28e1bd45b7719be46324cc47714e17bfc5
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-02-17 19:54:39 -0700

    working on inner lists

--- web/prove06/nav.php
+++ web/prove06/nav.php
@@ -1,9 +1,12 @@
 <?php
   echo '<nav>
-        <ul id="primaryNav">
-        <li><a href="login.php">CHANGE USER</a></li>
-        <li><a href="outerListView.php">LISTS</a></li>
-        <li><a href="addList.php">NEW LIST</a></li>
-        </ul>
-        </nav>';
+    <ul id="primaryNav">
+    <li><a href="login.php">CHANGE USER</a></li>
+    <li><a href="outerListView.php">LISTS</a></li>
+    <li><a href="addList.php">NEW LIST</a></li>'.
+    if(isset($_GET['listid'];)) {
+      echo '<li><a href="deleteListConfirm.php?listId='. $sublistid = $_GET['listid'] . '">DELETE LIST</a></li>';
+    }
+    . '</ul>
+    </nav>';
 ?>
