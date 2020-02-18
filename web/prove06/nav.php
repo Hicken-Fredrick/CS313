06cd73b535708bb66dc50c12c74b2b8b3f78341d
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-02-17 20:25:39 -0700

    working on inner lists

--- web/prove06/nav.php
+++ web/prove06/nav.php
@@ -6,7 +6,7 @@
     <li><a href="addList.php">NEW LIST</a></li>';
     
   if(isset($sublistid)) {
-    echo '<li><a href="deleteListConfirm.php?listid="$sublistid">DELETE LIST</a></li>';
+    echo '<li><a href="deleteListConfirm.php?listid="' . $sublistid . '">DELETE LIST</a></li>';
     }
 
   echo '</ul>
