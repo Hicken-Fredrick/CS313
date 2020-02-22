ce01b2fc0828957521e4dbde4cb61a649a3c1ba6
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-02-22 10:31:21 -0700

    deleteion scripts

--- web/prove06/deleteItemDB.php
+++ web/prove06/deleteItemDB.php
@@ -4,12 +4,12 @@ require('dbConnect.php');
 $db = get_db();
 session_start();
 
-$id = htmlspecialchars($_SESSION['user']);
-$listId = htmlspecialchars($_POST['listid']);
+$list = htmlspecialchars($_POST['listid']);
+$itemId = htmlspecialchars($_POST['itemid']);
 
-$stmt = $db->prepare('DELETE FROM wishlist.list WHERE listid= :listid AND userid= :id');
-$stmt->bindValue(':listid', $listId, PDO::PARAM_INT);
-$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt = $db->prepare('DELETE FROM wishlist.item WHERE itemid= :itemid AND listid= :listid');
+$stmt->bindValue(':listid', $list, PDO::PARAM_INT);
+$stmt->bindValue(':itemid', $itemId, PDO::PARAM_INT);
 $stmt->execute();
 
 die();
