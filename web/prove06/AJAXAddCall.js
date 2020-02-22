a19e652c3da9ec604a291bf15e0385bc8e92b5c1
Fred Hicken <35247195+Melrium@users.noreply.github.com>
2020-02-22 10:51:06 -0700

    deleteion scripts

--- web/prove06/AJAXAddCall.js
+++ web/prove06/AJAXAddCall.js
@@ -1,13 +1,13 @@
 $("#listAdd").click( function() {
   var data = $("#listInsert :input").serializeArray();
   $.post( $("#listInsert").attr("action"), data, function(info) { $("#listsList").append(info); });
-  //cleanupList();
+  cleanupList();
 });
 
 $("#itemAdd").click( function() {
   var data = $("#itemInsert :input").serializeArray();
   $.post( $("#itemInsert").attr("action"), data, function(info) { $("#itemsList").append(info); });
-  //cleanupItem();
+  cleanupItem();
 });
 
 $("#listInsert").submit( function() {
@@ -19,9 +19,9 @@ $("#itemInsert").submit( function() {
 });
 
 function cleanupList() {
-  $("#listInsert :input").each( function () { $(this).val(''); });
+  $("#listInsert").each( function () { this.reset(); });
 }
 
 function cleanupItem() {
-  $("#itemInsert :input").each( function () { $(this).val(''); });
+  $("#itemInsert").each( function () { this.reset(); });
 }
